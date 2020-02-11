c9f96cc2ca765618f74d073422f9481ed223cbe1
Your Name <kirikarankiri63@gmail.com>
2020-02-11 15:26:28 +0530

    form submision and validation

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,37 @@ router.get('/login',(req,res)=>res.render('login'));
 router.get('/register',(req,res)=>res.render('register'));
 //Register Handle
 router.post('/register',(req,res)=>{
-    console.log(req.body)
-    res.send('hello');
+
+    const{name,email,password,password2} =req.body;
+    let errors=[];
+
+    //check register field
+    if(!name || !email || !password|| !password2){
+        errors.push({msg:'please fill in the field '});
+    }
+
+    //check password match
+    if(password!==password2){
+        errors.push({msg:'password do not match'});
+    }
+
+    //check password lenght
+    if(password.length<6){
+        errors.push({msg:'password should be at least 6 characters'})
+    }
+
+    if(errors.length>0){
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    }
+    else{
+        res.send('pass');
+    }
 })
 
 module.exports=router;
