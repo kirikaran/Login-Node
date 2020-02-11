40b22f243890c8628dc99444a2f75ce2314d9581
Your Name <kirikarankiri63@gmail.com>
2020-02-11 11:39:14 +0530

    routes setup

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express =require('express');
 const app=express();
+//routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,console.log(`server started on port ${PORT}`));
