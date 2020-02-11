6452eea913427e42658d1fbae9899542e19f5da3
Your Name <kirikarankiri63@gmail.com>
2020-02-11 18:09:32 +0530

    logout and page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express =require('express');
 const router=express.Router();
+const {ensureAuthenticated}= require('../config/auth');
 //Wecome page
 router.get('/',(req,res)=>res.render('welcome'));
 
 //dashboard page
-router.get('/dashboard',(req,res)=>res.render('dashboard'));
+router.get('/dashboard',ensureAuthenticated,(req,res)=>res.render('dashboard',{
+    name:req.user.name
+}));
 module.exports=router;
