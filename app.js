114b7c786fbc62f246814a6fc57a4a529d381f7f
Your Name <kirikarankiri63@gmail.com>
2020-02-11 11:23:55 +0530

    create app.js file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express =require('express');
-const app=erxpress();
+const app=express();
 const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log('server started on port ${PORT}'));
+app.listen(PORT,console.log(`server started on port ${PORT}`));
